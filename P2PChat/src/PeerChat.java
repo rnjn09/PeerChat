fd5d05126cc0f848b11a3d1c1607e3fb258c3044
Ranjan Dhar <dharr@tcd.ie>
2015-12-12 19:48:55 +0000

    Added worker class

---
+++ P2PChat/src/PeerChat.java
@@ -0,0 +1,12 @@
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public interface PeerChat {
+
+	public void init(Socket socket, int uid);
+	public long joinNetwork(InetSocketAddress bootstrap_node);
+	public boolean leaveNetwork(long network_id);
+	public void chat(String text, String[] tags);
+	
+	
+}
